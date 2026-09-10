10cbe9335fa3e0c1a4de08a92db8f531b1643db5
Rajesh Gadari <rajesh.gadari2005@gmail.com>
2026-09-11 00:14:17 +0530

    intial commit

---
+++ init.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
+
+main()
+    .then((res) => {
+        console.log("connection successful");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+let allChats =[
+    {
+    from: "Neha",
+    to: "Priya",
+    msg: "send me your exam sheets",
+    created_at: new Date()
+    },
+
+    {
+    from: "Rohit",
+    to: "Mohit",
+    msg: "Teach me JS callbacks",
+    created_at: new Date()
+    },
+
+    {
+    from: "Amit",
+    to: "Sumit",
+    msg: "All the Best!",
+    created_at: new Date()
+    },
+
+    {
+    from: "Anita",
+    to: "Ramesh",
+    msg: "Bring me some fruits",
+    created_at: new Date()
+    },
+
+    {
+    from: "Tony",
+    to: "Peter",
+    msg: "Love you 3000",
+    created_at: new Date()
+    },
+]
+
+Chat.insertMany(allChats);
